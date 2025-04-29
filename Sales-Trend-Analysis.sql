486f52be5b0050beaade593c63912d15ca625295
swetha-pv <22hr1a3239@mtieat.org>
2025-04-29 20:53:40 +0530

    Update Sales-Trend-Analysis.sql

--- Sales-Trend-Analysis.sql
+++ Sales-Trend-Analysis.sql
@@ -39,3 +39,17 @@ ORDER BY
   month;
 
 
+
+SELECT
+  EXTRACT(YEAR FROM order_date) AS year,
+  EXTRACT(MONTH FROM order_date) AS month,
+  SUM(amount) AS total_revenue
+FROM
+  online_sales
+GROUP BY
+  EXTRACT(YEAR FROM order_date),
+  EXTRACT(MONTH FROM order_date)
+ORDER BY
+  total_revenue DESC
+LIMIT 3;
+
